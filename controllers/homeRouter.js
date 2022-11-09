6f2fecfb192112207d2f7a3c3bac52b87d859833
Ruxin Qu <rosheenqu@gmail.com>
2022-11-08 22:08:35 -0800

    add post time and user for each post

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -3,8 +3,17 @@ const sequelize = require('../config/connection')
 const { Post, User, Comment } = require('../models/index');
 
 homeRouter.get('/', async (req, res) => {
-    const post = await Post.findAll({ include: { model: User } })
-    const postArr = post.map(post => post.dataValues);
+    const post = await Post.findAll(
+        {
+            attributes: {
+                include: [
+                    [sequelize.fn('date_format', sequelize.col('createdAt'), '%m-%d-%Y'), 'posttime']
+                ],
+            },
+            include: { model: User }
+        })
+    const postArr = post.map((post) => post.get({ plain: true }));
+
     res.render('home', { postArr });
 })
 
