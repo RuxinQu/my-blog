9c3483696284403f3a4f52505a58cfece59aff55
Ruxin Qu <rosheenqu@gmail.com>
2022-11-10 23:40:11 -0800

    click on the title on dashboard page, user can edit the post

--- controllers/user/dashboard.js
+++ controllers/user/dashboard.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { isAuthenticated } = require('../../util/auth');
-const { Post, User } = require('../../models/index');
+const { Post, User, Comment } = require('../../models/index');
 
 router.get('/', isAuthenticated, async (req, res) => {
     try {
@@ -14,22 +14,15 @@ router.get('/', isAuthenticated, async (req, res) => {
                 include: { model: User }
             });
         const postArr = postData.map((post) => post.get({ plain: true }));
-        res.render('home', { postArr, login: req.isAuthenticated() });
+        res.render('dashboard', { postArr, login: req.isAuthenticated() });
     } catch (err) { console.error(err); }
 });
 
-router.get('/:id', isAuthenticated, async (req, res) => {
+router.get('/edit/:id', isAuthenticated, async (req, res) => {
     try {
-        const postData = await Post.findAll(
-            {
-                where: { user_id: req.session.passport.user },
-                attributes: {
-                    include: [[sequelize.fn('date_format', sequelize.col('createdAt'), '%m-%d-%Y'), 'posttime']],
-                },
-                include: { model: User }
-            });
-        const postArr = postData.map((post) => post.get({ plain: true }));
-        res.render('home', { postArr, login: req.isAuthenticated() });
+        const postData = await Post.findOne({ where: { id: req.params.id } });
+        const post = postData.get({ plain: true });
+        res.render('edit', { post, login: req.isAuthenticated() });
     } catch (err) { console.error(err); }
 });
 
--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model { };
+class Comment extends Model { }
 
 Comment.init(
     {
@@ -36,6 +36,6 @@ Comment.init(
         freezeTableName: true,
         modelName: 'comment',
     }
-)
+);
 
 module.exports = Comment;
