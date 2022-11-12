8d7cb5bbedfe4ac0a8842323452dec813d244aae
Ruxin Qu <rosheenqu@gmail.com>
2022-11-11 19:12:38 -0800

    click save button can add new post

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const isAuthenticated = require('../../util/auth');
 const { Post, User, Comment } = require('../../models/index');
 
+router.get('/newpost', isAuthenticated, async (req, res) => {
+    try {
+        res.render('newpost', { login: req.isAuthenticated() });
+    } catch (err) { console.error(err); }
+});
+
 router.get('/:id', isAuthenticated, async (req, res) => {
     try {
         const postData = await Post.findOne({
@@ -27,6 +33,19 @@ router.get('/edit/:id', isAuthenticated, async (req, res) => {
     } catch (err) { console.error(err); }
 });
 
+router.post('/', isAuthenticated, async (req, res) => {
+    const { title, content } = req.body;
+    const user_id = req.session.passport.user;
+    try {
+        await Post.create({
+            title,
+            content,
+            user_id
+        });
+        res.status(201).redirect('/user/dashboard');
+    } catch (err) { console.error(err); }
+});
+
 router.put('/:id', isAuthenticated, async (req, res) => {
     try {
         const updatePost = await Post.update(req.body, {
@@ -42,4 +61,5 @@ router.delete('/:id', isAuthenticated, async (req, res) => {
         res.status(204).send();
     } catch (err) { console.error(err); }
 });
+
 module.exports = router;
